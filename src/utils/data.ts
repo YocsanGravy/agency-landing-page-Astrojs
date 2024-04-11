483b6383d78d48b19a9b134a407627d7a77e9f94
YocsanGravy <2003sjmrm@gmail.com>
2024-04-10 22:17:29 -0400

    first commit

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,40 +1,41 @@
+
+
 const services = [
     {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        title: "Desarrollo de Sitios Web Personalizados",
+        description: "Creamos sitios web únicos, diseñados a medida para cumplir con los objetivos específicos de tu negocio, garantizando una presencia online impactante y funcional.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
+        title: "Diseño y Desarrollo de E-commerce",
+        description: "Desarrollamos tiendas en línea robustas y fáciles de administrar, optimizadas para ventas, con integraciones de sistemas de pago y gestión de inventario para una experiencia de compra sin fisuras.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        title: "Optimización de Motores de Búsqueda (SEO)",
+        description: "Mejoramos la visibilidad de tu sitio web en los motores de búsqueda, aplicando las mejores prácticas de SEO para atraer tráfico cualificado y aumentar las conversiones.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        title: "Mantenimiento y Soporte Web",
+        description: "Ofrecemos servicios continuos de mantenimiento y soporte para asegurar que tu sitio web permanezca actualizado, seguro y funcional frente a los desafíos digitales.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
+        title: "Desarrollo de Aplicaciones Web Progresivas (PWA)",
+        description: "Creamos aplicaciones web progresivas que ofrecen una experiencia de usuario similar a las aplicaciones nativas, pero sin las complicaciones de las tiendas de aplicaciones, para mejorar la participación y la retención de usuarios.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        title: "Branding y Diseño Gráfico",
+        description: "Desarrollamos la identidad visual de tu marca y creamos materiales de diseño gráfico cohesivos que comunican efectivamente el valor de tu negocio a tu audiencia objetivo.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
-    },
-];
+    },]
 
 export { services };
